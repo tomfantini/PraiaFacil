06f959fd8271982d2f541e6b7bc08f654f046aa3
tomfantini <cleyton.fantini@gmail.com>
2016-11-23 10:54:15 -0200

    commit inicial

---
+++ PraiaFacil/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PraiaFacil.Startup))]
+namespace PraiaFacil
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
